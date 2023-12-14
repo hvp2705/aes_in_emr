83c7311809ea173732b0f3b6df3a86c1f7d76ff0
hoangvietphuong2003 <92700113+hoangvietphuong2003@users.noreply.github.com>
2023-12-14 22:46:06 +0700

    remove sth

--- website/__init__.py
+++ website/__init__.py
@@ -17,7 +17,7 @@ def create_app():
     app.register_blueprint(views, url_prefix='/')
     app.register_blueprint(auth, url_prefix='/')
 
-    from .models import User, Note, Reminder
+    from .models import User
     
 
     return app
